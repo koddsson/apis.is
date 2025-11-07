090ced1088226440a8a72598bce5a42727af6e3c
Kristján Oddsson <koddsson@gmail.com>
2025-11-07 18:26:29 +0000

    add car endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { Router } from "./router.ts";
 
 import gengi from "./endpoints/gengi.ts";
 import meetups from "./endpoints/meetups.ts";
+import car from "./endpoints/car.ts";
 
 const router = new Router();
 router.add("GET", "/x/gengi/{:code}?", gengi);
 router.add("GET", "/x/meetups", meetups);
+router.add("GET", "/x/car/{:number}?", car);
 
 Deno.serve((...args) => router.route(...args));
