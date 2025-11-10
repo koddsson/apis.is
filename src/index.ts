dfc025f00f7f76e7aa37c606566507f5f02d0dfb
Kristján Oddsson <koddsson@gmail.com>
2025-11-10 20:09:40 +0000

    add race calendar

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import meta from "./endpoints/meta.ts";
 import gengi from "./endpoints/gengi.ts";
 import meetups from "./endpoints/meetups.ts";
 import car from "./endpoints/car.ts";
+import raceCalendar from "./endpoints/race-calendar.ts";
 
 const router = new Router();
+router.add("GET", "/", meta(router));
+
 router.add("GET", "/x/gengi/{:code}?", gengi);
 router.add("GET", "/x/meetups", meetups);
 router.add("GET", "/x/car/{:number}?", car);
-router.add("GET", "/", meta(router));
+router.add("GET", "/x/race-calendar", raceCalendar);
 
 Deno.serve((...args) => router.route(...args));
